e88ffcc2bdb648d630abf71b4f5bd6cb884fd54c
logancg14 <0997834@my.scccd.edu>
2025-11-06 22:24:13 -0800

    best wordle in the world

--- main.cc
+++ main.cc
@@ -1,6 +1,38 @@
 //Fill out this comment with your names and which bullet points you did
 //Partners:
-//Bullet Points: 
-//Extra Credit: 
-//URL to cover art and music: 
+//Bullet Points:
+//Extra Credit:
+//URL to cover art and music:
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <string>
+#include "/public/colors.h"
+//add dictionary later
 
+using namespace std;
+
+void puzzleWordle() {
+	int attempts = 0;
+	string guess;
+	cout << "Enter 5 letter word" << endl;
+	while (attempts < 6) {
+		cin >> guess;
+		if (guess.size() > 5 || guess.size() < 5) {
+			cout << "TRY AGAIN" << endl;
+			continue;
+		}
+		if (guess == "horse") {
+			cout << "YOU WIN!" << endl;
+			break;
+		} else {
+			cout << guess << endl;
+			//remember to add color to the letters here later
+		}
+		attempts = attempts + 1;
+	}
+}
+
+int main() {
+	puzzleWordle();
+}
