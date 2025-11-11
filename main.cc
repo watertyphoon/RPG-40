d8c2bdb7b13be8f7e36802c0898d91eba0d6a604
logancg14 <0997834@my.scccd.edu>
2025-11-10 21:42:27 -0800

    Small Map Update

--- main.cc
+++ main.cc
@@ -8,96 +8,122 @@
 #include <cmath>
 #include <string>
 #include "/public/colors.h"
-#include <thread>
-#include < limits>
-#include <chrono>
+#include <fstream>
+#include <sstream>
 //add dictionary later
 
 using namespace std;
 
 class Character {
-	private:
-		int hp;
-		int maxHp;
-	public:
-		//Public data
-		string name = "Among us";
-		string className = "Gambler";
-		int x, y;
-		int def;
-		int spd;
-		//Getters
-		int getHp() {return hp;}
-		int getMaxHp() {return maxHp;}
-		//Setters
-		void setHP(int newHp, bool ignoreMax = false) {
-			hp = newHp;
-			if(hp > maxHp && !ignoreMax) {hp = maxHp;}
-		}
-		void setMaxHp(int newMax) {
-			maxHp = newMax;
-			if(hp > maxHp) {hp = maxHp}
-		}
-
-		//Methods
-		void hurt(int damage, bool ignoreMax = false) { //Yes you can deal negative to heal the character.
-			hp -= damage;
-			if(hp > maxHp && !ignoreMax) {hp = maxHp;}
-		}
-		bool isDead() {return hp < 0;}
-
-		//Consturcter thing.
-		Character(int health = 100, string itsName = "NO NAME", int posX = -1, int  posY = -1;) {
-			maxHp = health;
+  private:
+	int hp;
+	int maxHp;
+  public:
+	//Public data
+	string name = "Among us";
+	string className = "Gambler";
+	int x, y;
+	int def;
+	int spd;
+	//Getters
+	int getHP() {
+		return hp;
+	}
+	int getMaxHp() {
+		return maxHp;
+	}
+	//Setters
+	void setHP(int newHp, bool ignoreMax = false) {
+		hp = newHp;
+		if (hp > maxHp && !ignoreMax) {
 			hp = maxHp;
-			name = itsName;
-			x = posX;
-			y = posY;
 		}
+	}
+	void setMaxHp(int newMax) {
+		maxHp = newMax;
+		if (hp > maxHp) {
+			hp = maxHp;
+		}
+	}
+
+	//Methods
+	void hurt(int damage, bool ignoreMax = false) { //Yes you can deal negative to heal the character.
+		hp -= damage;
+		if (hp > maxHp && !ignoreMax) {
+			hp = maxHp;
+		}
+	}
+	bool isDead() {
+		return hp < 0;
+	}
+
+	//Consturcter thing.
+	Character(int health = 100, string itsName = "NO NAME", int posX = -1, int  posY = -1) {
+		maxHp = health;
+		hp = maxHp;
+		name = itsName;
+		x = posX;
+		y = posY;
+	}
 };
 
 //lets say the character class has light attack of 5
-void lightAttack(Character& player, Character& enemy) { 
-	cout << "You sent out a light Attack!\n"; 
-	cout << "You did 5 attack damage to " << enemy.name << endl; 
+void lightAttack(Character& player, Character& enemy) {
+	cout << "You sent out a light Attack!\n";
+	cout << "You did 5 attack damage to " << enemy.name << endl;
 	enemy.hurt(5);
-} 
+}
 
 
 //lets say the character class has light attack of 8
-void heavyAttack(Character& player, Character& enemy) { 
-	cout << "You sent out a heavy Attack!\n"; 
-	cout << "You did 8 attack damage to " << enemy.name << endl; 
+void heavyAttack(Character& player, Character& enemy) {
+	cout << "You sent out a heavy Attack!\n";
+	cout << "You did 8 attack damage to " << enemy.name << endl;
 	enemy.hurt(8);
 }
 
 
 void menu(const Character& player) { //placeholder function will expand upon further later...
 	int temp;
-	cout << "1) Stats (press 1)\n"; 
-	cout << "2) Inventory (press 2)\n" 
+	cout << "1) Stats (press 1)\n";
+	cout << "2) Inventory (press 2)\n";
 	cout << "3) Quit (press 3)\n";
-	set_raw_made(false);
-	while(true) {
+	set_raw_mode(false);
+	while (true) {
 		cin >> temp;
 		clearscreen();
-		movecursor(0,0);
-		if(temp == 1) { //waiting for the character class to be uploaded :p
-			cout << "Name: " << player.getName() << endl; 
-			cout << "Class: " << player.getClass() << endl; //needs to be implemented, just return string of the class type "Knight"
-			cout << "HP: " << player.getHP() "/" << player.getMaxHp() << endl; 
-			cout << "Defense: " << player.getDef() << endl; 
-			cout << "Speed: " << player.getSpd() << endl; //needs to be implemented 
+		movecursor(0, 0);
+		if (temp == 1) { //waiting for the character class to be uploaded :p
+			cout << "Name: " << player.name << endl;
+			//cout << "Class: " << player.class << endl; //needs to be implemented, just return string of the class type "Knight"
+			//cout << "HP: " << player.getHP() "/" << player.getMaxHp() << endl;
+			//cout << "Defense: " << player.getDef() << endl;
+			//cout << "Speed: " << player.getSpd() << endl; //needs to be implemented
+		} else if (temp == 2) {
+			cout << "Items in Inventory: \n";
+			cout << "1) Map\n";
+			//cout << "2) " << player.getWeapon() << endl; //needs to be implemented
+			cout << "3) Healing Potion (+5 HP)" << endl;
+		} else {
+			break;
 		}
-		else if (temp == 2) { 
-			cout << "Items in Inventory: \n"; 
-			cout << "1) Map\n"; 
-			cout << "2) " << player.getWeapon() << endl; //needs to be implemented
-			cout << "3) Healing Potion (+5 HP)" << endl; 
-		}
-		else {break;}
 	}
-	
+
+}
+
+vector <string> mapCreation() {
+	vector <string> mapData;
+	ifstream map("Maps.csv");
+	string temp;
+	while (map >> temp) {
+		for (int i = 0; i < temp.size(); i++) {
+			if (temp.find(",") != string::npos) {
+				temp.replace(temp.find(","), 1, "");
+			}
+		}
+		mapData.push_back(temp);
+	}
+	return mapData;
 }
 
 
@@ -124,6 +150,8 @@ void puzzleWordle() {
 
 
 int main() {
-
-	puzzleWordle();
+	vector <string> MAP = mapCreation();
+	for (int i = 0; i < MAP.size(); i++) {
+		cout << MAP.at(i) << endl;
+	}
 }
